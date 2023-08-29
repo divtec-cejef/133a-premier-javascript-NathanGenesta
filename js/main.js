629cf7020ddb5dca50409c33e580c3e8ef7435dd
genenat <nathan.genesta@divtec.ch>
2023-08-29 16:18:59 +0200

    Ajout l'article dans l'ul

--- js/main.js
+++ js/main.js
@@ -8,9 +8,14 @@
 
 // Récupère tous les paragraphes du document
 let tabPara = document.querySelectorAll("p");
-
-//Récupérer le premier bouton avec la classe "Ajouter"
-let btAjouter = document.querySelectorAll("button.ajouter");
-
-//Affiche le contenu des variables dans la console
-console.log(tabPara, btAjouter);
+let btAjouter = document.querySelector("button.ajouter");
+let txtArticle = document.querySelector("#article");
+let ulArticle = document.querySelector("ul.articles");
+// Affiche le contenu des variables dans la console
+console.log(tabPara, btAjouter, txtArticle, ulArticle);
+// Ecoute le click sur btAjouter
+btAjouter.addEventListener("click", () => {
+    ulArticle.innerHTML += "<li>" + txtArticle.value + "</li>";
+    // Vide le champ texte
+    txtArticle.value = "";
+});
